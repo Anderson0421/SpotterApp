1e265b51183d37baf37d90a4a86a58d5d55b49ca
TheJp202 <jeanpierrellc.jpllc@gmail.com>
2024-04-13 19:51:23 -0500

    Pequeñas actualizaciones 1

--- core/settings.py
+++ core/settings.py
@@ -27,11 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+DEBUG = 'RENDER' not in os.environ
 
 # Application definition
 
@@ -46,18 +48,20 @@ INSTALLED_APPS = [
     'Estacionamiento',
     'Post',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
@@ -147,3 +151,18 @@ AZURE_SAS_TOKEN=os.environ.get('AZURE_SAS_TOKEN')
 
 MEDIA_URL ='https://%s.blob.core.windows.net/%s/' % (AZURE_ACCOUNT_NAME, AZURE_CONTAINER)
 MEDIA_ROOT='http://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
