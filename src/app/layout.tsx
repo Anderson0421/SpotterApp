5aa68032f7c6c2d6d70c68de64302f0f3653609e
Anderson0421 <jelow0413@gmail.com>
2024-04-13 21:40:01 -0500

    pendiente como mostrar la seccion del blog (features) y crear los tipos para el fetch de datos. se cambiaron algunas animaciones para el homepage principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-
+import Navbar from '@/components/Navbar';
 
 const inter = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -18,6 +18,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
   return (
     <html lang="en" className={inter.className}>
       <body>
+        <Navbar />
         {children}
         <Footer />
       </body>
---
+++ src/types/type.d.ts
@@ -0,0 +1,9 @@
+// Aca colocaremos los tipos de datos con Typescript para la app
+
+export type Post = {
+    id : number
+    PostTitulo : string
+    PostDescripcion : string
+    PostPortada : string
+    PostFechaCreacion : string
+}
