eb13eee6278b9e3f15040534d6e187fbc1efb6a2
TheJp202 <jeanpierrellc.jpllc@gmail.com>
2024-04-13 20:01:18 -0500

    Pequeñas actualizaciones 2

--- core/settings.py
+++ core/settings.py
@@ -166,3 +166,13 @@ CORS_ALLOW_METHODS = [
     'POST',
     'PUT',
 ]
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
